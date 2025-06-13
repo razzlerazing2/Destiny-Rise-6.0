f9e1ce87517618f460acc8bf06a9153a8ee92638
razzlerazing2 <deepvishwakarma037@gmail.com>
2025-06-13 22:06:25 +0000

    qwasdf

--- sixty_nine/sonicboom.js
+++ sixty_nine/sonicboom.js
@@ -1,3 +1,3 @@
-setTimeout(function () {
-    alert("Welcome to SkellySKript, where unblocking possibilities are endless. This was created by Iforgor_ES/@razzlerazing2 & @467riguy8. Enjoy!");
-}, 2000);
+/* setTimeout(function () {
+     alert("Welcome to SkellySKript, where unblocking possibilities are endless. This was created by Iforgor_ES/@razzlerazing2 & @467riguy8. Enjoy!");
+}, 2000);*/
