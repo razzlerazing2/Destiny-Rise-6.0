32625f384bb7bd5d927b4b7ff3f8c26a1573f2e8
razzlerazing2 <deepvishwakarma037@gmail.com>
2025-06-13 19:09:18 +0000

    dasd a

--- sixty_nine/sonicboom.js
+++ sixty_nine/sonicboom.js
@@ -1,8 +1,3 @@
-function aboutblank() {
-            var win = window.open('about:blank');
-            var url = 'Index.html';
-            var iframe = win.document.createElement('iframe');
-            iframe.style = "position:fixed;width:100vw;height:100vh;top:0px;left:0px;right:0px;bottom:0px;z-index:2147483647;background-color:white;border:none;";
-            iframe.src = url;
-            win.document.body.appendChild(iframe);
-        }
+setTimeout(function () {
+    alert("Welcome to SkellySKript, where unblocking possibilities are endless. This was created by Iforgor_ES/@razzlerazing2 & @467riguy8. Enjoy!");
+}, 2000);
