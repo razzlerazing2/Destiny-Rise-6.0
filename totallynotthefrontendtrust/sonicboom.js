ff9dc509110d03c85fe9b4732af4da276605b47d
razzlerazing3 <deepthereepskelly@gmail.com>
2025-07-19 22:05:39 +0000

    sd wasdawg

--- totallynotthefrontendtrust/sonicboom.js
+++
@@ -1,3 +0,0 @@
-/* setTimeout(function () {
-     alert("Welcome to SkellySKript, where unblocking possibilities are endless. This was created by Iforgor_ES/@razzlerazing2 & @467riguy8. Enjoy!");
-}, 2000);*/
